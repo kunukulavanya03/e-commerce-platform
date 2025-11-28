e17b9e4bd8a2e8f3d2578f96a259b2be9809ccc9
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 16:39:39 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Contact.module.css';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
+    </div>
+  );
+}
