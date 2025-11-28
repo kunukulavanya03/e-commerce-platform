3fb823345455d75f40ae4767a61cc4608edcd700
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 16:39:28 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './About.module.css';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
+    </div>
+  );
+}
