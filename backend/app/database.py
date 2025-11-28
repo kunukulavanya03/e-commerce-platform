153e0028fb24d42dfd5e607609876428ac4bbd7b
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 16:38:22 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ backend/app/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models import Base
+
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://user:password@localhost/db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(engine)
