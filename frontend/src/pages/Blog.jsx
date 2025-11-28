071f6772a8dc456a36ce73bd5abecfefd7dea542
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 16:39:33 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/src/pages/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Blog.module.css';
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
+    </div>
+  );
+}
